3ddd20a885ba34cbb6de733aefdccf467e9e2d77
pranav5127 <pranav.kumar.5127@gmail.com>
2025-11-26 17:40:25 +0530

    create search bar

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Label, Header, Input
-
+from textual.widgets import Header, Input
 
 class CmdHistoryApp(App):
     TITLE = "CmdHistory"
     SUB_TITLE = "A Tool to easily search previously executed commands in the terminal."
+    CSS_PATH = "app.tcss"
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Input(placeholder="type here")
+        yield Input(placeholder="🔎 Ctrl+k", id="search")
 
 if __name__ == "__main__":
     app = CmdHistoryApp().run()
